8bca32c662d3e6a9df54ed491e3790eac89a7c80
AugustusB <A.Balasuriya@btinternet.com>
2018-05-02 16:04:29 +0100

    initial creation

---
+++ scripts/ops.js
@@ -0,0 +1,13 @@
+(function(ns1, ns2){
+    ns2.OpsCls = function OpsCls (){
+        // Private methods
+        
+        // Public methods
+        return{
+
+        };
+
+        // Public Properties
+    }
+
+})(window.Gus = window.Gus || {}, window.GusJsOperators = window.Gus.JsOperators || {})
